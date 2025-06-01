962710895bb581c3a2229db55224dda866f6fc3f
Patricia Costa <patriciacostapatriciac@outlook.com.br>
2025-06-01 12:25:24 -0300

    Implementação do método para buscar por titulo.

--- src/main/java/com/redeleitura/controller/LivroController.java
+++ src/main/java/com/redeleitura/controller/LivroController.java
@@ -1,20 +1,30 @@
 package com.redeleitura.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.redeleitura.entity.LivroAtual;
 import com.redeleitura.entity.LivrosLidos;
-import com.redeleitura.entity.Usuario;
 import com.redeleitura.service.LivroService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import com.redeleitura.util.GoogleBooksUtil;
 
 @RestController
 @RequestMapping("/api/livros")
 public class LivroController {
 
     private final LivroService livroService;
+    private final GoogleBooksUtil googleBooksUtil;
 
-    public LivroController(LivroService livroService) {
+    public LivroController(LivroService livroService, GoogleBooksUtil googleBooksUtil) {
         this.livroService = livroService;
+        this.googleBooksUtil = googleBooksUtil;
     }
 
     @PostMapping("/{idUsuario}/lido/{isbn}")
@@ -29,6 +39,11 @@ public class LivroController {
         return ResponseEntity.ok(livroAtual);
     }
 
-    // TODO Método para inserção de livro atual
+     @GetMapping("/buscar")
+    public ResponseEntity<List<GoogleBooksUtil.LivroDTO>> buscarLivrosPorTitulo(@RequestParam String titulo) { 
+        List<GoogleBooksUtil.LivroDTO> resultados = googleBooksUtil.buscarLivrosPorTitulo(titulo);
+        return ResponseEntity.ok(resultados); 
+    }
+    
 }
 
